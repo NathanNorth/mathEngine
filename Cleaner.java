050e4d5d1ae4e01f13b5cbb6b9970e9bac083ca4
qExobyte <74025759+qExobyte@users.noreply.github.com>
2020-12-26 14:10:59 -0500

    Update Cleaner.java

--- Cleaner.java
+++ Cleaner.java
@@ -1,15 +1,18 @@
 public class Cleaner {
-   
+
    public static String highLevelClean(String in) { 
       //replaces two character operators with single character alternatives to make other methods run smoother
       in = in.replace(">=", "~");
       in = in.replace("<=", "`");
       
-      if(Processor.indexOfOperator(in) == -1) {
-         //eric code zone
-         
+      if (Processor.indexOfOperator(in) == -1) {
          return "noSign";
+      }   
+      if (in.endsWith("+") || in.endsWith("-") || in.endsWith("*") || in.endsWith("/")) {
+         System.out.println("detecting operator at end");
+         in = in.substring(0, in.length() - 1);
       }
       return in;
    }
+
 }
