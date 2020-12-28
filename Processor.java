4a12bac1939b6bb07db2449e9261722a16036b5a
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2020-12-28 14:58:43 -0500

    demon code from hell that works for even numbers sometimes

--- Processor.java
+++ Processor.java
@@ -7,20 +7,9 @@ public class Processor {
       
       //GUESS AND CHECK ALGORYTHM
       if(in.contains("x") && index != 0 && index != in.length() - 1) {
-         for(double i = 0; i < Double.MAX_VALUE; i++) {
-            String guess = in.replace("x", "(" + i + ")");
-            Equation eq1 = new Equation(guess);
-            if(eq1.result) {
-               return "" + i;
-            }
-            
-            String guessNegative = in.replace("x", "(" + -i + ")");
-            Equation eq2 = new Equation(guessNegative);
-            if(eq2.result) {
-               return "" + -i;
-            }
-         }
+         return "" + solveFor("x", in, 1000, 0);
       }
+      
       if(in.contains("x")) { //this only runs if there is an x and a unfinished expression
          return "";
       }
@@ -37,6 +26,53 @@ public class Processor {
       else {
          Equation eq1 = new Equation(in);
          return "" + eq1.result;
+      }
+   }
+   
+   //REWRITE TO HAVE START RANGE END RANGE YAAAAS
+   private static double solveFor(String letter, String in, double times, double start) {
+      Double[] guesses = new Double[20];
+      int pos = 0;
+      
+      //currently checks positives and negatives because im dumb and hate my life
+      for(double i = 0; i < 10; i ++) {
+         double plug = (i + start) * times / 10; //this is cringe and hard to trage
+         
+         String guess = in.replace(letter, "(" + plug + ")");
+         Equation eq1 = new Equation(guess);
+         guesses[pos] = eq1.difference;
+         pos++;
+            
+         String guessNegative = in.replace(letter, "(" + -plug + ")");
+         Equation eq2 = new Equation(guessNegative); 
+         guesses[pos] = eq2.difference;
+         pos++;
+      } //NOTE LAST ITEM OF ARRAY MIGHT BE SCUFFED
+      
+      double bestGuess = times;
+      int bestAPos = -1;
+      for(int i = 0; i < guesses.length; i++) {
+         if(Math.abs(guesses[i]) < bestGuess) {
+            bestGuess = Math.abs(guesses[i]);
+            bestAPos = i;
+         }
+      }
+      
+      //attrocious demon code from hell becuse my array is needlessly complicated
+      double realI = 0;
+      if(bestAPos % 2 == 1) {
+         realI =  -(bestAPos - 1) / 2;
+      }
+      else {
+         realI = bestAPos / 2;
+      }
+      
+      realI = (realI + start) * times / 10;  //recreates the plug value which is the actual x val we care about
+      if(bestGuess == 0) {
+         return realI;
+      }
+      else {
+         return solveFor("x", in, times / 10, realI);
       }
    }
    
