d6442ebe84bcf89d75ba778a600e1a33467a3b44
Kieran <kierank.pc@gmail.com>
2020-12-28 20:59:00 -0500

    Simplified gui

--- GuiDriver.java
+++ GuiDriver.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 public class GuiDriver {
     public static void main(String args[]) {
-        JFrame frame = new JFrame("My First GUI");
+        JFrame frame = new JFrame("Math Engine (v0.3141592653589793238462643383279502)");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 100);
+        frame.setSize(300, 130);
         frame.getContentPane().add(new Gui()); // Adds Button to content pane of frame
         frame.setVisible(true);
     }
