2ba2d239c31a712b99c502e3c5b31b4fd020a72d
qExobyte <74025759+qExobyte@users.noreply.github.com>
2020-12-30 17:47:05 -0500

    Update Cleaner.java

--- Cleaner.java
+++ Cleaner.java
@@ -1,17 +1,30 @@
 public class Cleaner {
 
    public static String highLevelClean(String in) { 
-      //replaces two character operators with single character alternatives to make other methods run smoother
+      // replaces two character operators with single character alternatives to make other methods run smoother
       in = in.replace(">=", "~");
       in = in.replace("<=", "`");
       
+      // input has no = < > <= >=
+
       if (Processor.indexOfOperator(in) == -1) {
-         return "noSign";
+         // Ends with operators, operator gets chopped
+         if (in.endsWith("+") || in.endsWith("-") || in.endsWith("*") || in.endsWith("/") || in.endsWith("^")) {
+            in = in.substring(0, in.length() - 1);
+         }
+
+         // Consecutive operators return nosign
+         if (in.contains("++") || in.contains("**") || in.contains("//") || in.contains("^^") || in.substring(0, 2).equals("--")) {
+            return "noSign";
+         }
+
+         // Make Parenthesis Method, looking at pTable (but not using it) for info
+            // Start parenthesis but no end fills in end and returns in
+            // End parenthesis but no start returns nosign
+
+         // * or / or ^ at start returns nosign
+
       }   
-      if (in.endsWith("+") || in.endsWith("-") || in.endsWith("*") || in.endsWith("/")) {
-         System.out.println("detecting operator at end");
-         in = in.substring(0, in.length() - 1);
-      }
       return in;
    }
 
