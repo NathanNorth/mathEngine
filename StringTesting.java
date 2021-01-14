c74e086bfc6b052576672b9c5d77e6ac1fb02070
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-01-14 14:41:04 -0500

    test

--- StringTesting.java
+++ StringTesting.java
@@ -4,7 +4,7 @@ import expressions.*;
 public class StringTesting {
    
    public static void main(String[] args) {
-      //Debug 2
+      //Debug
       expressions.Expression pog = expressions.Expression.parse("2*(1+1*(13+6))");
       System.out.println(pog);
    
